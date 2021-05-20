e0e7e908dcb2a0739dc12f23794482bbfadd7747
emmavanderhaeghen <emmavand24@student.arteveldehs.be>
2021-05-20 20:27:14 +0200

    last fixes and changes

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -9,6 +9,7 @@
 	(factory((global.bootstrap = {}),global.jQuery,global.Popper));
 }(this, (function (exports,$,Popper) { 'use strict';
 
+
 $ = $ && $.hasOwnProperty('default') ? $['default'] : $;
 Popper = Popper && Popper.hasOwnProperty('default') ? Popper['default'] : Popper;
 
